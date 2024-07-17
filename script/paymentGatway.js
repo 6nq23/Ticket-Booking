e70f639ef1063ebf99128174b7b0747e5396fe2e
Hitesh <hiteshsuthar8255@gmail.com>
2024-07-17 00:13:52 -0700

    fix: adding total prise of tickets in header of paymentGatway page

--- script/paymentGatway.js
+++ script/paymentGatway.js
@@ -1,8 +1,10 @@
+const headerPrise = document.getElementById('total-prise-in-header');
 const paySecurlyRate = document.getElementById('pay-securly-rate');
 
 function changeData(){
   const tempTicketDetails = JSON.parse(localStorage.getItem('tempTicketDetails'))
-  paySecurlyRate.innerText = localStorage.getItem('prise')*tempTicketDetails.numberOfTickets
+  headerPrise.innerText = localStorage.getItem('prise')*tempTicketDetails.numberOfTickets;
+  paySecurlyRate.innerText = localStorage.getItem('prise')*tempTicketDetails.numberOfTickets;
 }
 
 window.onload = changeData();
